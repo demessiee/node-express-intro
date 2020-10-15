c650cc3a724022c492a8bd3e7e48ea42201f17d7
Benjamin Lin <blin@Benjamins-MacBook-Pro.local>
2020-10-15 16:09:28 -0700

    add starter code sample in 2A

--- Labs/Lab-2A-Starter/index.js
+++ Labs/Lab-2A-Starter/index.js
@@ -7,6 +7,34 @@ const port = 3000
 app.use("/static",express.static(path.join(__dirname, 'public')))
 
 
+
+/* example to read from a JSON file, parse the incoming data into a JSON object, 
+modify the data then convert the modified data into a JSON stringify 
+before writing it out to a file
+
+fs.readFile('./public/employee_data.json', (err, data) => {
+    if (err) throw err;
+    let json = JSON.parse(data); //parse file data into json object
+    json = json.slice(0,5) //modify json object
+    let data = JSON.stringify(json, null, 2); //convert modified json object into JSON string
+    fs.writeFile('./public/employee_data.json', data, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+        res.json(json) //send back modified data as a response to the client
+    });
+});
+*/
+
+app.get('/api/employee/id/:id', (req, res) => {
+    fs.readFile('./public/employee_data.json', (err, data) => {
+        if (err) throw err;
+        let json = JSON.parse(data);
+        let result = json.filter(x => x._id === req.params.id)
+        res.json(result)
+    });
+})
+
+
 app.get('/api/employee/id/:id', (req, res) => {
     fs.readFile('./public/employee_data.json', (err, data) => {
         if (err) throw err;
