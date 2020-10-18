e8cd39ffd83f574cd445f191b1dcf2f827e7b2b8
Brad <brad@learnwebcode.com>
2020-10-17 18:20:06 -0700

    Fixed code erors when deleting employee from database.

--- Labs/Lab-2C-Starter/index.js
+++ Labs/Lab-2C-Starter/index.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 
 
-mongoose.connect('mongodb://localhost:27017/test',{ useNewUrlParser: true }).then( () => {
+mongoose.connect('mongodb://localhost:27017/test',{ useNewUrlParser: true, useUnifiedTopology: true }).then( () => {
   app.use("/static",express.static(path.join(__dirname, 'public')))
 
   app.use('/api',employees)
