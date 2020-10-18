a100c4aff52cbb3a0bb805d42441e08216c4dd7c
Brad <brad@learnwebcode.com>
2020-10-18 16:04:13 -0700

    Fix starter code for 1D

--- Labs/Lab-1D-Starter/index.js
+++ Labs/Lab-1D-Starter/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,7 +7,7 @@ const port = 3000
 app.get('/', (req, res) => {
   fs.readFile('./public/employee_data.json', (err, data) => {
     if (err) throw err;
-    let json = JSON.parse(data); //JSON.parse will parse the contents of the file into json object you can use
+    let result = JSON.parse(data); //JSON.parse will parse the contents of the file into json object you can use
     res.json(result)
   });
 })
