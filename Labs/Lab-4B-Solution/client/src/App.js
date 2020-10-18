b504c4cf5719ab29efecde32987b398ca159225d
Benjamin Lin <blin@Benjamins-MacBook-Pro.local>
2020-10-17 17:54:04 -0700

    lab 4B done

---
+++ Labs/Lab-4B-Solution/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from './components/Carousel.js'
+
+function App() {
+  return (
+    <div>
+        <Carousel/>
+    </div>
+  );
+}
+
+export default App;
